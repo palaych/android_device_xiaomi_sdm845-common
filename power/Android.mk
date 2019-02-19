77dbeb41f2b63fc4e06872a837c9713aa280702d
palaych <palaychm@yandex.ru>
2019-02-19 23:43:21 +0000

    sdm845-common: power: use $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
    LOCAL_HEADER_LIBRARIES don't support

--- power/Android.mk
+++ power/Android.mk
@@ -24,7 +24,7 @@ LOCAL_C_INCLUDES := external/libxml2/include \
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
 
-LOCAL_HEADER_LIBRARIES := generated_kernel_headers
+$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include := generated_kernel_headers
 
 LOCAL_CFLAGS += -Werror -Wall -Wno-unused-parameter
 LOCAL_CFLAGS += -DINTERACTION_BOOST
