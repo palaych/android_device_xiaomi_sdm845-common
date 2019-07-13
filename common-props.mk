7fcfdd85311d0858ac7d20eb4cf79b319da22aae
palaych <palaychm@yandex.ru>
2019-07-13 20:16:59 +0000

    sdm845-common: clean props

--- common-props.mk
+++ common-props.mk
@@ -37,8 +37,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.ad=1 \
     ro.vendor.display.ad.hdr_calib_data=/vendor/etc/hdr_config.cfg \
     ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
-    ro.vendor.display.sensortype=2 \
-    persist.displayfeature.extcolor.proc=0
+    ro.vendor.display.sensortype=2
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
